424cdbb2d58a734281c40c9bd4553414e1e8093c
shri hari <shrihari2818@gmail.com>
2025-09-03 19:45:56 +0530

    Add files via upload

---
+++ Clocktimer.py
@@ -0,0 +1,24 @@
+import time 
+import winsound
+
+
+
+while True:
+    try:
+        seconds = int(input("⏱️ enter the time in seconds: "))
+        if seconds<1:
+            print("Please enter a number greater than 0")
+            continue
+        break
+    except ValueError:
+        print("Invalid input Please enter a whole number")
+
+print("\n🔔 Timer started....")
+for remaning in range(seconds,0,-1):
+    mins,secs = divmod(remaning,60)
+    time_formate = f"{mins:02}:{secs:02}"
+    print(f"⏱️ Time left: {time_formate} ",end="\r")
+    time.sleep(1)
+
+print("\n Time's up! Take a break or move to next task.")
+winsound.Beep(1000, 500)
